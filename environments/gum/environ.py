113ea9bedd0419acb69800bc5e73ee3c1a03a858
Douglas Hanley <thesecretaryofwar@gmail.com>
2025-12-09 15:47:51 -0600

    hook up fool image eval system

--- environments/gum/environ.py
+++ environments/gum/environ.py
@@ -6,7 +6,8 @@ import verifiers as vf
 from PIL import Image
 from datasets import Dataset, load_dataset
 
-from .client import GumError, ErrorType, renderGum
+import oneping
+from gum import render, GumError, GumErrorType
 
 ##
 ## notes
@@ -20,16 +21,25 @@ from .client import GumError, ErrorType, renderGum
 
 # starvector/text2svg-stack: has lots of diagram-like SVG images with CoG-VLM generated descriptions
 
+# would be cool to make an ascii-art environment to test out vision based evals
+
 ##
 ## system prompt
 ##
 
-SYSTEM_PROMPT = 'You are an assistant that writes gum.jsx code given a text description.'
+SYSTEM_GENERATE = 'You are an assistant that writes gum.jsx code given a text description.'
+
+##
+## constants
+##
+
+RENDER_PIXELS = 512
 
 ##
 ## reward components
 ##
 
+REWARD_BOXED = 1
 REWARD_CODE = 1
 REWARD_PARSE = 1
 REWARD_GENERATE = 1
@@ -43,20 +53,49 @@ REWARD_MASK = 1
 ##
 
 def extract_code(text):
-    match = re.search(r'```jsx\n(.*?)\n```', text, re.DOTALL)
+    match = re.search(r'```([^\n]*)\n(.*?)\n```', text, re.DOTALL)
     if match:
-        return match.group(1).strip()
-    return ''
+        code = match.group(2).strip()
+        return code, True
+    else:
+        return text, False
 
 ##
 ## eval functions
 ##
 
-def eval_text_image(prompt, data):
-    pass
+EVALUATE_PROVIDER = 'llama-cpp'
+EVALUATE_SYSTEM = """Given a user request and a generated PNG image, evaluate how well the image satisfies the prompt. Use the following rubric to evaluate the image. Each of these 5 criteria is worth 1 point:
 
-def eval_image_image(prompt, data):
-    pass
+- Content is fully visible
+- Relevant to the prompt
+- Satisfies the prompt
+- Aesthetically acceptable
+- Text (if any) is legible (if no text is expected, give 1 point)
+
+First, assess each criterion individually. Then give your final response as "SCORE: <score>", where <score> is an integer between 0 and 5."""
+
+REGEX_SCORE = r'SCORE: (\d+)'
+
+def eval_image(query, data, debug=False):
+    # get the lvlm reply
+    prompt = f'Assess the following image given the user query: {query}.'
+    response = oneping.reply(prompt, image=data, system=EVALUATE_SYSTEM, provider=EVALUATE_PROVIDER)
+
+    # print debug info
+    if debug:
+        print(f'[DEBUG] full eval response:\n\n{response}')
+
+    # try to parse the score
+    try:
+        match = re.search(REGEX_SCORE, response)
+        assert match is not None
+        score = int(match.group(1))
+        assert score >= 0 and score <= 5
+        return score
+    except Exception as e:
+        print(f'[WARNING] score extraction failed: {e}')
+        return 0
 
 ##
 ## reward functions
@@ -66,38 +105,48 @@ def reward_gum(prompt, text):
     reward = 0
 
     # extract code from text
-    code = extract_code(text)
+    code, boxed = extract_code(text)
+
+    # bonus for boxed code
+    if boxed:
+        reward += REWARD_BOXED
+
+    # empty code somehow
+    if len(code) == 0:
+        return reward
 
     # parse code to svg
+    data = None
+    etype = None
     try:
-        data = renderGum(code)
-        etype = None
+        data = render(code, pixels=RENDER_PIXELS)
     except GumError as e:
-        data = None
         etype = e.error_type
+    except Exception as e:
+        etype = GumErrorType.UNKNOWN
 
     # reward based on error type
-    if etype == ErrorType.UNKNOWN:
+    if etype == GumErrorType.UNKNOWN:
         pass
-    if etype == ErrorType.NOCODE:
+    if etype == GumErrorType.NOCODE:
         pass
     reward += REWARD_CODE
-    if etype == ErrorType.NORETURN:
+    if etype == GumErrorType.NORETURN:
         return reward
     reward += REWARD_RETURN
-    if etype == ErrorType.NOELEMENT:
+    if etype == GumErrorType.NOELEMENT:
         return reward
     reward += REWARD_ELEMENT
-    if etype == ErrorType.PARSE:
+    if etype == GumErrorType.PARSE:
         return reward
     reward += REWARD_PARSE
-    if etype == ErrorType.GENERATE:
+    if etype == GumErrorType.GENERATE:
         return reward
     reward += REWARD_GENERATE
-    if etype == ErrorType.RENDER:
+    if etype == GumErrorType.RENDER:
         return reward
     reward += REWARD_RENDER
-    if etype == ErrorType.UNKNOWN:
+    if etype == GumErrorType.UNKNOWN:
         return reward
 
     # no data? return reward
@@ -105,8 +154,13 @@ def reward_gum(prompt, text):
         return reward
 
     # convert png data to image
-    image = Image.open(io.BytesIO(data))
-    pixels = np.asarray(image)
+    try:
+        image = Image.open(io.BytesIO(data))
+        pixels = np.asarray(image)
+        assert pixels.ndim == 3
+    except Exception as e:
+        print(f'[WARNING] pixel conversion failed: {e}')
+        return reward
 
     # check if its empty
     pixel_mask = pixels.mean(axis=-1) / 255
@@ -186,7 +240,7 @@ def load_environment(
     # set up environment
     return vf.SingleTurnEnv(
         dataset=dataset,
-        system_prompt=SYSTEM_PROMPT,
+        system_prompt=SYSTEM_GENERATE,
         parser=parser,
         rubric=rubric,
         **kwargs,
